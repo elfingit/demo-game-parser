fc9b34c39d277e5b2a20b5f2526a9a8392c49067
Andrey Sergeev <andreylong@gmail.com>
2019-07-08 10:46:05 +0300

    small fix

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const mainRouter = require('./routes/main');
 app.use('/api', mainRouter);
 
 const port = process.env.APP_PORT || 3000;
+const host = process.env.APP_HOST || '127.0.0.1';
 
-app.listen(port);
+app.listen(port, host);
 
 console.log('Server is listening on http://localhost:' + port);
